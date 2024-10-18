b26e6217480873456887c410ed4241351faa7de0
Karl's WSL <karl.weihmann@gmail.com>
2024-10-18 22:08:47 +0200

    quick-commit

--- src/f_load_images.c
+++ src/f_load_images.c
@@ -6,7 +6,7 @@
 /*   By: kweihman <kweihman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:17:38 by kweihman          #+#    #+#             */
-/*   Updated: 2024/10/15 07:36:45 by kweihman         ###   ########.fr       */
+/*   Updated: 2024/10/18 22:08:15 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,33 @@
 void	*mlx_xpm_file_to_image(void *mlx_ptr, char *filename,
 			       int *width, int *height);
 
+/*Helper function for f_load_images. Loads an image from a file and saves its
+pointer to the image pointer passed as argument. Returns 0 on success and -1
+on failure.*/
+static int f_helper(t_game *game, void *image, char *filename)
+{
+	int dummy1;
+	int dummy2;
+
+	image = mlx_xpm_file_to_image(game->mlx_ptr, filename, &dummy1, &dumm2);
+	if (image == NULL)
+		return (-1);
+	return (0);
+}
+
 /*Loads six predefined images and saves their pointers to game struct.
 Returns 0 on success and -1 on failure.*/
 int f_load_images(t_game *game)
 {
-	game->image->collectible =
-	 mlx_xpm_file_to_image(game->mlx_ptr, "collectible.xpm", NULL, NULL);
-	if (game->image->collectible == NULL)
-		return (-1);
-	game->image->player =
-	 mlx_xpm_file_to_image(game->mlx_ptr, "player.xpm", NULL, NULL);
-	if (game->image->player == NULL)
-		return (-1);
-	game->image->background =
-	 mlx_xpm_file_to_image(game->mlx_ptr, "background.xpm", NULL, NULL);
-	if (game->image->background == NULL)
-		return (-1);
-	game->image->wall =
-	 mlx_xpm_file_to_image(game->mlx_ptr, "wall.xpm", NULL, NULL);
-	if (game->image->wall == NULL)
-		return (-1);
-	game->image->exit_open =
-	 mlx_xpm_file_to_image(game->mlx_ptr, "exit_open.xpm", NULL, NULL);
-	if (game->image->exit_open == NULL)
-		return (-1);
-	game->image->exit_closed =
-	 mlx_xpm_file_to_image(game->mlx_ptr, "exit_closed.xpm", NULL, NULL);
-	if (game->image->exit_closed == NULL)
+	if (f_helper(game, game->image->collectible, "collectible.xpm") == -1 ||
+	    f_helper(game, game->image->player, "player.xpm") == -1 ||
+	    f_helper(game, game->image->background, "background.xpm") == -1 ||
+	    f_helper(game, game->image->wall, "wall.xpm") == -1 ||
+	    f_helper(game, game->image->exit_open, "exit_open.xpm") == -1 ||
+	    f_helper(game, game->image->exit_closed, "exit_closed.xpm") == -1)
 		return (-1);
 	return (0);
 }
+
+// WIP: I need to think about whether I am passing the image pointer correctly.
+// Also I need to think about how I handle the path to the image file.
