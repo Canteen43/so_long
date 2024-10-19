049c947392b4dccad48c56137784c5107735c99c
Karl's WSL <karl.weihmann@gmail.com>
2024-10-19 09:59:49 +0200

    quick-commit

--- src/f_load_images.c
+++ src/f_load_images.c
@@ -6,7 +6,7 @@
 /*   By: kweihman <kweihman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:17:38 by kweihman          #+#    #+#             */
-/*   Updated: 2024/10/18 22:08:15 by kweihman         ###   ########.fr       */
+/*   Updated: 2024/10/19 09:59:33 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,21 @@
 void	*mlx_xpm_file_to_image(void *mlx_ptr, char *filename,
 			       int *width, int *height);
 
+// From Libft
+char	*ft_strjoin(char const *s1, char const *s2);
+
 /*Helper function for f_load_images. Loads an image from a file and saves its
 pointer to the image pointer passed as argument. Returns 0 on success and -1
 on failure.*/
-static int f_helper(t_game *game, void *image, char *filename)
+static int f_helper(t_game *game, void *image, char *filepath)
 {
 	int dummy1;
 	int dummy2;
 
-	image = mlx_xpm_file_to_image(game->mlx_ptr, filename, &dummy1, &dumm2);
-	if (image == NULL)
+	filepath = ft_strjoin("assets/sprites/", filepath);
+	*image = mlx_xpm_file_to_image(game->mlx_ptr, filename, &dummy1, &dumm2);
+	free(filepath);
+	if (*image == NULL)
 		return (-1);
 	return (0);
 }
@@ -34,12 +39,12 @@ static int f_helper(t_game *game, void *image, char *filename)
 Returns 0 on success and -1 on failure.*/
 int f_load_images(t_game *game)
 {
-	if (f_helper(game, game->image->collectible, "collectible.xpm") == -1 ||
-	    f_helper(game, game->image->player, "player.xpm") == -1 ||
-	    f_helper(game, game->image->background, "background.xpm") == -1 ||
-	    f_helper(game, game->image->wall, "wall.xpm") == -1 ||
-	    f_helper(game, game->image->exit_open, "exit_open.xpm") == -1 ||
-	    f_helper(game, game->image->exit_closed, "exit_closed.xpm") == -1)
+	if (f_helper(game, &(game->image->collectible), "collectible.xpm") == -1 ||
+	    f_helper(game, &(game->image->player), "player.xpm") == -1 ||
+	    f_helper(game, &(game->image->background), "background.xpm") == -1 ||
+	    f_helper(game, &(game->image->wall), "wall.xpm") == -1 ||
+	    f_helper(game, &(game->image->exit_open), "exit_open.xpm") == -1 ||
+	    f_helper(game, &(game->image->exit_closed), "exit_closed.xpm") == -1)
 		return (-1);
 	return (0);
 }
